797dc409b1dc37ea5d808d2696113bfb4d59bc43
George Czabania <george@czabania.com>
2016-12-24 12:21:19 +1300

    Add lib/merge

---
+++ test/merge/shallow.js
@@ -0,0 +1,33 @@
+import test from 'ava'
+
+import shallowMerge from '../../lib/merge/shallow'
+
+test('does what you would expect', (t) => {
+  const oldValue = {
+    a: 1,
+    b: 2,
+    c: 3,
+  }
+
+  const newValue = {
+    a: 100,
+  }
+
+  const result = shallowMerge(newValue, oldValue)
+
+  t.deepEqual(result, {
+    a: 100,
+    b: 2,
+    c: 3,
+  })
+
+  t.deepEqual(newValue, {
+    a: 100,
+  })
+
+  t.deepEqual(oldValue, {
+    a: 1,
+    b: 2,
+    c: 3,
+  })
+})
